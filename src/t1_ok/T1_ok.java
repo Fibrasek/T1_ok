f8de8c4e7b7488692eddada592b3714d6e67ed2a
Fibrasek <gabriel1490@gmail.com>
2013-09-29 14:13:52 -0300

    Alteração do Header

--- src/t1_ok/T1_ok.java
+++ src/t1_ok/T1_ok.java
@@ -13,7 +13,6 @@
  */
 package t1_ok;
 
-import java.io.Console;
 import java.util.Scanner;
 
 public class T1_ok {
@@ -34,8 +33,21 @@ public class T1_ok {
         System.out.println("2 - Procurar um veículo;");
         System.out.println("3 - Remover um veículo;");
         System.out.println("4 - Listar veículos no sistema.");
-        System.out.println("5 - Sair :(");
         functions(escolha());
+    }
+
+    private static void limpar_console() {
+        try {
+            String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (Exception exception) {
+            //  Handle exception.
+        }
     }
 
     static public String escolha() {
@@ -45,6 +57,8 @@ public class T1_ok {
     }
 
     static public void functions(String escolha) {
+        String aux = null;
+        
         Record carro;
         int num_chassi, ano;
         String nome, modelo, marca;
@@ -52,27 +66,45 @@ public class T1_ok {
         switch (escolha) {
             case "1":
                 System.out.println("=== CADASTRAR NOVO VEÍCULO ===");
-                System.out.print("Número do Chassi (7 Dígitos): ");
-                num_chassi = Integer.parseInt(escolha());
+                           
+                do{
+                    System.out.print("Número do Chassi (min. 3 Dígitos): ");
+                    aux = escolha();
+                    if(aux.length() < 3){ System.out.println("ERRO: Valor em formato errado.");}
+                }while(aux.length() < 3 );
+                
+                num_chassi = Integer.parseInt(aux);
+                
                 System.out.print("Nome: ");
                 nome = escolha();
+                
                 System.out.print("Modelo: ");
                 modelo = escolha();
+                
                 System.out.print("Marca: ");
                 marca = escolha();
-                System.out.print("Ano: ");
-                ano = Integer.parseInt(escolha());
+                
+                do{
+                    System.out.print("Ano: ");
+                    aux = escolha();
+                    if(aux.length() < 4){ System.out.println("ERRO: Valor em formato errado.");}
+                }while(aux.length() < 4 );
+                
+                ano = Integer.parseInt(aux);
 
                 carro = new Record(num_chassi, nome, modelo, marca, ano);
                 hs.insert(carro);
+                System.out.print("Pressione Enter para voltar ao menu.");
+                escolha();
                 
+
                 print_menu();
                 break;
             case "2":
                 System.out.println("=== PESQUISAR VEICULO ===");
                 System.out.print("Digite o número do chassi: ");
                 num_chassi = Integer.parseInt(escolha());
-                
+
                 hs.search_record(num_chassi);
                 print_menu();
                 break;
@@ -80,18 +112,10 @@ public class T1_ok {
                 System.out.println("=== REMOVER VEICULO ===");
                 System.out.print("Digite o número do chassi: ");
                 num_chassi = Integer.parseInt(escolha());
-                
+
+
                 hs.remove(num_chassi);
                 print_menu();
-                break;
-            case "4":
-                System.out.println("=== LISTANDO ITENS CADASTRADOS ===");
-                
-                hs.print_hash_table();
-                print_menu();
-                break;
-            case "5":
-                System.out.println("=== VOLTE SEMPRE ===");
                 break;
         }
     }
